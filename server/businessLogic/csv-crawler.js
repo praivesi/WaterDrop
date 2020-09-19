7532949be9dba6baf83dc12933c38d07b2ca0e02
praivesi <praivesi@gmail.com>
2020-09-19 19:34:59 +0900

    Add parsing CSV data into MindStat struct

--- server/businessLogic/csv-crawler.js
+++ server/businessLogic/csv-crawler.js
@@ -1,6 +1,9 @@
 const fs = require('fs')
 const results = []
 const iconv = require('iconv-lite')
+const MindStat = require('../models/mind-statistics-model')
+  
+
 
 function loadCSV()
 {
@@ -8,13 +11,37 @@ function loadCSV()
     var data = fs.readFileSync(path, {encoding: "binary"})
     var data2EUCKR = iconv.decode(data, 'euc-kr')
     var rows = data2EUCKR.split("\n")
+    var column = rows[1].split(',')
 
-    for (var rowIndex in rows) {
-        // TODO: Parse row and create data filled list
-        results.push(rows[rowIndex])
+    var lifeStatisfyColumnIdx = -1
+
+    for (i = 0; i < column.length; i++) {
+        if (column[i] === "삶의 만족도 (점)") {
+            lifeStatisfyColumnIdx = i;
+            break;
+        }
     }
 
-    console.log(results)
+    if (lifeStatisfyColumnIdx === -1) return;
+
+    // for (var rowIndex in rows) 
+    for (i = 2; i < rows.length; i++) {
+        var row = rows[i].split(',')
+        var newMStat = new MindStat()
+
+        newMStat.country = row[0]
+        newMStat.lifeSatisfication = row[lifeStatisfyColumnIdx]
+
+        results.push(newMStat)
+    }
+
+    results.sort(function(a, b){
+        return b.lifeSatisfication - a.lifeSatisfication;
+    })    
+
+    for (i = 0; i < results.length; i++) {
+        console.log("COUNTRY: " + results[i].country + ", LIFE STATIFACTION: " + results[i].lifeSatisfication)
+    }
 }
 
 module.exports =
