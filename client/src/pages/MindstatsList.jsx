c7ffcaa4b225519bbdb6c35c1a84ec82c2ccd428
praivesi <praivesi@gmail.com>
2020-10-04 02:08:20 +0900

    Add using react-map-gl for Map display

--- client/src/pages/MindstatsList.jsx
+++ client/src/pages/MindstatsList.jsx
@@ -1,4 +1,6 @@
-import React, { Component } from 'react'
+import React, { Component, useState } from 'react'
+import ReactMapGL from 'react-map-gl'
+import 'mapbox-gl/dist/mapbox-gl.css';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
 import api from '../api'
 
@@ -9,8 +11,9 @@ const Wrapper = styled.div`
     padding: 0 40px 40px 40px;
 `
 
+
 class MindstatsList extends Component {
-    constructor(props){
+    constructor(props) {
         super(props)
         this.state = {
             mindstats: [],
@@ -19,8 +22,8 @@ class MindstatsList extends Component {
         }
     }
 
-    componentDidMount = async() => {
-        this.setState({isLoading: true})
+    componentDidMount = async () => {
+        this.setState({ isLoading: true })
 
         console.log("Before getMindstats")
 
@@ -34,10 +37,9 @@ class MindstatsList extends Component {
         })
     }
 
-    render(){
+    render() {
         const { mindstats, isLoading } = this.state
         console.log('TCL: MindStatsList -> render -> mindstats', mindstats)
-
 
         console.log(mindstats)
 
@@ -55,20 +57,42 @@ class MindstatsList extends Component {
         ]
 
         let showChart = true
-        if(!mindstats.length){
+        if (!mindstats.length) {
             showChart = false
         }
+
+        console.log('showChart: ' + showChart)
+        /*
+        const [viewport, setViewport] = useState({
+            latitude: 37,
+            longitude: 127,
+            width: '100vw',
+            height: '100vh',
+            zoom: 12
+        })
+        */
 
         return (
             <Wrapper>
                 {showChart && (
-                    <LineChart width={500} height={300} data={mindstats}>
-                        <XAxis dataKey="name" />
+                    /*
+                    <LineChart width={5000} height={300} interval={0} data={mindstats}>
+                        <XAxis dataKey="country" />
                         <YAxis />
                         <CartesianGrid stroke="#eee" strokeDasharray="5 5" />
-                        <Line type="monotone" dataKey="country" stroke="#8884d8" />
-                        <Line type="monotone" dataKey="lifeSatisfication" stroke="#82ca9d" />
+                        <Line type="monotone" dataKey="lifeSatisfication" stroke="#8884d8" />
                     </LineChart>
+                    */
+                    //<ReactMapGL {...viewport}>
+                    <ReactMapGL
+                        width={'80vw'}
+                        height={'80vh'}
+                        mapStyle='mapbox://styles/mapbox/light-v9'
+                        //mapboxApiAccessToken='hide token for privacy'
+                        latitude={37}
+                        longitude={127}
+                        zoom={1}
+                    />
                 )}
             </Wrapper>
         )
@@ -76,4 +100,3 @@ class MindstatsList extends Component {
 }
 
 export default MindstatsList
-        
